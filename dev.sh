e2d566a4a2477e0fbdfeeae99d194737e342d45c
Stephan Gammeter <sgammeter@gmail.com>
2020-12-29 22:51:32 +0100

    Add default compute capabilities.

--- dev.sh
+++ dev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# -u $(id -u):$(id -g) \
-cd devenv
+
+cd development_image
 make deploy
+
+mkdir -p /home/geli/{tf_gpu_dev_out,tf_gpu_dev_src}
 
 docker pull 192.168.0.14:5000/tf-gpu-env
 docker run \
@@ -9,5 +11,7 @@ docker run \
   --add-host buildfarm-server:192.168.0.14 \
   --add-host buildfarm-redis:192.168.0.14 \
   -v /home/geli/.bazel_in_docker:/home/geli/.bazel_in_docker \
+  -v /home/geli/tf_gpu_dev_out:/mnt \
+  -v /home/geli/tf_gpu_dev_src:/home/geli/tf_gpu_dev_src \
   192.168.0.14:5000/tf-gpu-env \
   bash
--- up.sh
+++ up.sh
@@ -55,25 +55,3 @@ docker run -d \
   192.168.0.14:5000/tf-gpu-builder \
   java -Djava.util.logging.config.file=/config/prod.logging.properties -jar buildfarm-shard-worker_deploy.jar /config/shard-worker-16.config --public_name=192.168.0.11:8981
 EOF
-
-# docker run -it -p 8980:8980 --add-host=build-server:192.168.0.14  192.168.0.14:5000/foo:latest bash
-# docker run -d \
-#   -p 6379:6379 \
-#   --add-host buildfarm-server:192.168.0.14 \
-#   redis:5.0.9
-
-# docker run \
-#   --add-host buildfarm-server:192.168.0.14 \
-#   -p 8980:8980 \
-#   -v sshvolume:/config \
-#   192.168.0.14:5000/tf-gpu-builder \
-#   java -Djava.util.logging.config.file=/config/prod.logging.properties -jarbuildfarm-server_deploy.jar /config/shard-server.config --port=8980 --public_name=192.168.0.14:8980 -Djava.util.logging.config.file=/config/prod.logging.properties
-
-# docker run \
-#   --add-host buildfarm-server:192.168.0.14 \
-#   --add-host buildfarm-redis:192.168.0.14 \
-#   -p 8981:8981 \
-#   -v sshvolume:/config \
-#   -v /tmp/worker:/tmp/worker \
-#   192.168.0.14:5000/tf-gpu-builder \
-#   java -Djava.util.logging.config.file=/config/prod.logging.properties -jar:buildfarm-shard-worker_deploy.jar /config/shard-worker.config --public_name=192.168.0.15:8981 -Djava.util.logging.config.file=/config/prod.logging.properties
