451962a677ec26dbb3f446bfb0f1a36bb028767e
Stephan Gammeter <sgammeter@gmail.com>
2020-12-29 11:16:15 +0100

    Backup

--- dev.sh
+++ dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+docker pull 192.168.0.14:5000/tf-gpu-env
 docker run \
   -it --rm \
   --add-host buildfarm-server:192.168.0.14 \
   --add-host buildfarm-redis:192.168.0.14 \
-  -v /home/geli:/home/geli \
+  -v /home/geli/.bazel_in_docker:/home/geli/.bazel_in_docker \
   192.168.0.14:5000/tf-gpu-env \
   bash
