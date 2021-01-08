5741c67c372ccee4756a8d246e7dab22415ec309
Stephan Gammeter <sgammeter@gmail.com>
2021-01-08 18:49:57 +0100

    Map /tmp in bazel docker

--- bin/start_env.sh
+++ bin/start_env.sh
@@ -26,6 +26,7 @@ docker run \
   --add-host buildfarm-redis:192.168.0.14 \
   -v /home/geli/.bazel_in_docker:/home/geli/.bazel_in_docker \
   -v $PWD:$PWD \
+  -v /tmp:/tmp \
   192.168.0.14:5000/tf-gpu-env \
   bash > .bazel_docker_id
 
