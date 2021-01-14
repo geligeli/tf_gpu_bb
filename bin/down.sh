ab80226d40cb626cf7d8101009f27c3a05af4a64
Stephan Gammeter <sgammeter@gmail.com>
2021-01-14 10:38:28 +0100

    Update hosts

--- bin/down.sh
+++ bin/down.sh
@@ -1,13 +1,3 @@
-ssh geli@192.168.0.14 bash << 'EOF'
-docker stop $(docker ps -a | grep -v registry | sed '1 d' | awk '{print $1}')
-docker rm $(docker ps -a | grep -v registry | sed '1 d' | awk '{print $1}')
-EOF
-
-ssh geli@192.168.0.15 bash << 'EOF'
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-EOF
-
 ssh geli@192.168.0.10 bash << 'EOF'
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
@@ -17,3 +7,19 @@ ssh geli@192.168.0.11 bash << 'EOF'
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 EOF
+
+ssh geli@192.168.0.13 bash << 'EOF'
+docker stop $(docker ps -a | grep -v registry | sed '1 d' | awk '{print $1}')
+docker rm $(docker ps -a | grep -v registry | sed '1 d' | awk '{print $1}')
+EOF
+
+ssh geli@192.168.0.14 bash << 'EOF'
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+EOF
+
+ssh geli@192.168.0.15 bash << 'EOF'
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+EOF
+
